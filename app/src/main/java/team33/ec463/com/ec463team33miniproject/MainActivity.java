b640c268c872f0a1e1b3becb223742a04be6f857
thuchngu <thucnguyen61098@gmail.com>
2019-09-08 01:18:38 -0400

    Initial commit

---
+++ app/src/main/java/team33/ec463/com/ec463team33miniproject/MainActivity.java
@@ -0,0 +1,14 @@
+package team33.ec463.com.ec463team33miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
